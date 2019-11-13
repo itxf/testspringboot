8e41289779f30877e5961d599bbcfa88f5ffe886
itxf <1443882747@qq.com>
2019-11-13 21:24:29 +0800

    减去一个toString

--- ssm/src/main/java/cn/xdl/bean/et_admin.java
+++ ssm/src/main/java/cn/xdl/bean/et_admin.java
@@ -39,10 +39,7 @@ public class et_admin implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	@Override
-	public String toString() {
-		return "et_admin [id=" + id + ", name=" + name + ", password=" + password + "]";
-	}
+	
 	
 	
 
